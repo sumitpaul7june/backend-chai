34430c989b5ae4006b93fdea0cf0283f54cf6b1b
Sumit <73896616+sumitpaul7june@users.noreply.github.com>
2026-02-15 19:35:30 +0530

    add register controller and bug fixes

--- src/index.js
+++ src/index.js
@@ -1,51 +1,22 @@
-// require('dotenv').config({ path: './env' });
 import dotenv from "dotenv";
-
-import mongoose, { connect } from "mongoose";
-import express from "express";
 import connectDB from "./db/index.js";
+import { app } from "./app.js"; // IMPORT 'app' FROM APP.JS
 
-const app = express();
 dotenv.config({
     path: './env'
-})
-
+});
 
 connectDB()
     .then(() => {
         app.on('error', (error) => {
-            console.log(('Error', error));
+            console.log('Server Error: ', error);
             throw error;
+        });
 
-        })
         app.listen(process.env.PORT || 8000, () => {
-            console.log(`Server is running at port ${process.env.PORT}`);
-
-        })
+            console.log(`Server is running at port : ${process.env.PORT}`);
+        });
     })
     .catch((err) => {
-        console.log("MONGODB CONNECTION FAILED", err);
-
-    })
-
-
-// import express from "express";
-// ; (async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//         app.on("error", (error) => {
-//             console.log("Error", error);
-//             throw error;
-//         })
-
-//         app.listen(process.env.PORT, () => {
-//             console.log(`App is listening on port ${process.env.PORT}`);
-
-//         })
-//     }
-//     catch (error) {
-//         console.error("Error", error);
-//         throw err;
-
-//     }
-// })();
+        console.log("MONGODB CONNECTION FAILED !!! ", err);
+    });
